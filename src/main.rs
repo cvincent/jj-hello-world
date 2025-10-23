d584456079766b52d4644f9ef0a5672e0c32d6c2
Chris Vincent <c.j.vincent@protonmail.com>
2025-10-23 07:32:19 -0500

    Split hello/goodbye

--- src/main.rs
+++ src/main.rs
@@ -3,5 +3,14 @@
 /// I forgot about Rust doc syntax.
 
 fn main() {
+    print_hello();
+    print_goodbye();
+}
+
+fn print_hello() {
     println!("Hello, world!");
+}
+
+fn print_goodbye() {
+    println!("Goodbye, world!");
 }
